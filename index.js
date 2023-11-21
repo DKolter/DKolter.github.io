9f4565eb5574f78dbe1e91f1ef04d6321f383485
DKolter <danielkolter157@gmail.com>
2023-11-21 22:33:12 +0100

    Navigation

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const copyDone = document.getElementById("copy-done");
 const contactButton = document.getElementById("contact-button");
 const modal = document.getElementById("modal");
 const modalContent = document.getElementById("modal-content");
+const navigationCheck = document.getElementById("navigation-check");
+const navigation = document.getElementById("navigation");
 
 copyButton.addEventListener("click", copyEmailToClipboard);
 contactButton.addEventListener("click", () => modal.style.display = "flex");
 modal.addEventListener("click", () => modal.style.display = "none");
 modalContent.addEventListener("click", (event) => event.stopPropagation());
+navigationCheck.addEventListener("click", () => navigation.classList.toggle("navigation-open"));
 
 function copyEmailToClipboard() {
     navigator.clipboard.writeText(modalEmail.innerText);
